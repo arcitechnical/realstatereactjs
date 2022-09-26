4d3719a181b733cc476afb98614d360e743bf6cb
windows <windows@microsoft.com>
2022-09-26 12:42:48 +0530

    pull data

--- src/component/Program.js
+++ src/component/Program.js
@@ -31,17 +31,6 @@ function Myprogram()
         <button onClick={mychang}>change value</button>
         <input type="text" value={first} />
         <h1>{b}</h1>
-<<<<<<< HEAD
-        <select >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-        </select>
-        <marquee scrollamount={first}> moving text</marquee>
-=======
->>>>>>> da185fab9bec104f4d540417220b41e39fcff812
         
         <marquee scrollamount={first}> moving text</marquee>
         <div className="container">
