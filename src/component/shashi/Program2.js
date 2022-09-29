146f6b18088206ef33f92d8ee7d7a5f1645d6362
Archita <arcishub1311@gmail.com>
2022-09-29 13:14:58 +0530

    pull the data

--- src/component/shashi/Program2.js
+++ src/component/shashi/Program2.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import './custom.css';
 
 function MyProgram()
-
-
 {
     const [x,y] = useState('color')
     const [settitle,gettitle] = useState('ducat')
@@ -16,42 +14,32 @@ function MyProgram()
         gettitle(e.target.title);
     }
 
-const mycolor = (e)=>
-{
-    getcolor(e.target.value);
-}
+    const mycolor = (e)=>
+    {
+        getcolor(e.target.value);
+    }
+    const openmodal = (event)=>
+    {
+        updatemodal('block');
 
-
-const openmodal = (event)=>
-{
-    updatemodal('block');
-
-}
-const myclose = ()=>
-{
-    updatemodal('none');
-}
-
-    
-
+    }
+    const myclose = ()=>
+    {
+        updatemodal('none');
+    }
     return(
         <>
-        <h1 style={{color:`${setcolor}`}}>This is program 2 file</h1>
-        <input type={x} title={settitle}  onChange={mycolor} />
-        <button type="button" title="this is color" onClick={a}>submit</button>
-        
-        <input type="button" value="open modal" onClick={openmodal} />
-
-        <div className="mymodal" style={{display:`${openm}`}}>
-            <h1>This is modal</h1>
-            <p> welcome to this modal</p>
-            <input type="text" />
-            <button className="close" onClick={myclose}>X</button>
-
-        </div>
-
+            <h1 style={{color:`${setcolor}`}}>This is program 2 file</h1>
+            <input type={x} title={settitle}  onChange={mycolor} />
+            <button type="button" title="this is color" onClick={a}>submit</button>
+            <input type="button" value="open modal" onClick={openmodal} />
+            <div className="mymodal" style={{display:`${openm}`}}>
+                <h1>This is modal</h1>
+                <p> welcome to this modal</p>
+                <input type="text" />
+                <button className="close" onClick={myclose}>X</button>
+            </div>
         </>
     )
 }
-
 export default MyProgram
--- src/index.js
+++ src/index.js
@@ -2,25 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //import Myprogram from "./component/Program";
-<<<<<<< HEAD
-import Layout from "./component/archita/Layout";
-import {BrowserRouter} from 'react-router-dom';
-import MyProgram from "./component/shashi/Program1";
-
-
-=======
 //import Layout from "./component/archita/Layout";
 import { BrowserRouter } from "react-router-dom";
-//import MyProgram from "./component/shashi/Program2";
+import MyProgram from "./component/shashi/Program2";
 import Home from "./component/neha/Home";
->>>>>>> 719b557e60a57492658e9e96eb1ae823b7c16230
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Home />
-      {/* <MyProgram /> */}
+      <MyProgram /> 
+      <Home />
       {/* <Layout /> */}
     </BrowserRouter>
   </React.StrictMode>
