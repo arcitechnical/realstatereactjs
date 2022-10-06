2a673a4951a528120fba03f80c8626ab700fda0e
Archita <arcishub1311@gmail.com>
2022-10-06 22:05:45 +0530

    pull the data

--- src/component/shashi/Program3.js
+++ src/component/shashi/Program3.js
@@ -2,33 +2,23 @@ import React, { useState } from "react";
 
 function Myprogram3()
 {
-
-    const [getvalue1,updatvalue] = useState('')
-    const [msg,msgupdate] = useState('block')
-let abc = (e)=>
-{
-    
-    updatvalue(e.target.value);
-
-    if(getvalue1 ==='')
-    {
-
-        console.log(getvalue1);
-        msgupdate('none');
+        const [getvalue1,updatvalue] = useState('')
+        const [msg,msgupdate] = useState('block')
+    let abc = (e)=>
+    { 
+        updatvalue(e.target.value);
+        if(getvalue1 ==='')
+        {
+            console.log(getvalue1);
+            msgupdate('none');
+        }
     }
-    
-}
-
     return(
         <>
-        <h1>welcome</h1>
-        <input type="text" value={getvalue1} onChange={abc}/>
-        <p style={{color:'red',display:`${msg}`}}>tis is blank text box</p>
-        
-        
+            <h1>welcome</h1>
+            <input type="text" value={getvalue1} onChange={abc}/>
+            <p style={{color:'red',display:`${msg}`}}>tis is blank text box</p> 
         </>
     )
 }
-
-
 export default Myprogram3
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 // import Myprogram from "./component/Program";
 //import Layout from "./component/archita/Layout";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 // import MyProgram from "./component/shashi/Program2";
 import Myprogram3 from "./component/shashi/Program3";
 // import Home from "./component/neha/Home";
